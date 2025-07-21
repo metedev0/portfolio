ca74d4b5b91f8fb32aa39af80ee73e85876198fa
metedev0 <83536252+metedev0@users.noreply.github.com>
2025-07-21 19:10:05 +0300

    Update Experience.tsx

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -9,39 +9,23 @@ import { JobImages } from "@/components/JobImages";
 const jobs = [
     {
         role: "Senior Full-Stack Developer", // TODO: Replace with actual role
-        company: "Tech Innovators Inc.", // TODO: Replace with actual company name
+        company: "Metedev Inc.", // TODO: Replace with actual company name
         logo: "/company.svg", // TODO: Replace with actual logo
-        duration: "2020 - Present",
+        duration: "2022 - Presents",
         description:
             "Lead developer for multiple high-impact projects, mentoring junior developers, and implementing best practices.",
-        link: "https://techinnovators.com",
+        link: "#",
         images: [],
     },
     {
-        role: "Senior Full-Stack Developer", // TODO: Replace with actual role
-        company: "Tech Innovators Inc.", // TODO: Replace with actual company name
+        role: "Senior Frontend Developer", // TODO: Replace with actual role
+        company: "Linkedin Inc.", // TODO: Replace with actual company name
         logo: "/company.svg", // TODO: Replace with actual logo
-        duration: "2018 - 2020",
+        duration: "2020 - 2022",
         description:
             "Developed and maintained various client projects, focusing on responsive design and performance optimization.",
-        link: "https://websolutions.com",
-        images: [
-            "/devvault.png",
-            "/devvault.png",
-        ],
-    },
-    {
-        role: "Senior Full-Stack Developer", // TODO: Replace with actual role
-        company: "Tech Innovators Inc.", // TODO: Replace with actual company name
-        logo: "/company.svg", // TODO: Replace with actual logo
-        duration: "2016 - 2018",
-        description:
-            "Assisted in the development of web applications, gained experience in agile methodologies and version control.",
-        link: "https://startupventures.com",
-        images: [
-            "/devvault.png",
-            "/devvault.png",
-        ],
+        link: "https://linkedin.com",
+        images: [],
     },
 ]
 
